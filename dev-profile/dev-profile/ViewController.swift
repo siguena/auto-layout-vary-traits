5ba5daaa93f941c9488ff4bbd498f8377200628f
Vladimir Milushev <vladimirmilushev@Vladimirs-MacBook-Air.local>
2017-10-21 21:22:48 +0300

    Changes from wednesday

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainLogo.layer.cornerRadius = 8
+        mainLogo.clipsToBounds = trues
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var mainLogo: UIImageView!
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
